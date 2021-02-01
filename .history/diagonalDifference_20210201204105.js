075e886a6dd6dd82fd549cc95410947626c46637
Syed abdul rahim <syedabdulrahim4@gmail.com>
2021-02-01 20:47:25 +0530

    first commit

---
+++ .history/diagonalDifference_20210201204105.js
@@ -0,0 +1,31 @@
+
+function diagonalDifference(arr) {
+    // Write your code here
+    let j=0;
+
+    
+   const len=inputString[0];
+    let k=inputString[0]-1;
+   
+   let sumA=0;
+   let sumB=0;
+   let testArr=[];
+   
+   for(let i=0;i<arr.length;i++){
+       sumA=sumA+arr[i][i];
+       sumB=sumB+arr[j][k];
+       testArr.push(arr[j][k]);
+       ++j;
+       --k;
+       
+   }
+   
+//    for(let j=0;j<arr.length;j++){
+       
+//        sumB=sumB+arr[k][];
+//        testArr.push(arr[i][i]);
+//    }
+   
+   
+   return Math.abs(sumA-sumB);
+}
